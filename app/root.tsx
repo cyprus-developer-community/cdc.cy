d0d9af7fb6727b5893b6f9c1fda29d1b7ddd7b21
Patrick Heneise <patrick@zentered.co>
2022-09-14 22:30:28 +0300

    feat: add mailerlite overlay

--- app/root.tsx
+++ app/root.tsx
@@ -41,6 +41,17 @@ export default function App() {
         />
         <link rel="manifest" href="/site.webmanifest"></link>
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+            .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+            n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+            (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+            ml('account', '19898');
+        `
+          }}
+        />
       </head>
       <body className="antialiased font-sans bg-white">
         {/* <Header /> */}
