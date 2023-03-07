44023ea7c1ec758106134b700efa6d05c7cf86e0
msimou <marios.simouu@gmail.com>
2023-03-07 11:34:23 +0200

    feat: improve e2e coverage (#80)

--- e2e/events.spec.ts
+++ e2e/events.spec.ts
@@ -11,4 +11,50 @@ test.describe('Events', () => {
     ).toBeVisible()
     await expect(page).toHaveTitle('Cyprus Developer Community | CDC.cy')
   })
+
+  test.describe('Upcoming events', () => {
+    test('should show the correct title', async ({ page }) => {
+      await expect(
+        page
+          .getByTestId('upcoming-events-section')
+          .getByRole('heading', { name: 'Upcoming events', level: 2 })
+      ).toBeVisible()
+    })
+
+    test('should select an upcoming event', async ({ page }) => {
+      const upcomingEvent = page
+        .getByTestId('upcoming-events-section')
+        .getByTestId('event-card')
+        .first()
+      const upcomingEventTitle = await upcomingEvent
+        .getByRole('heading')
+        .textContent()
+      await upcomingEvent.click()
+      await expect(
+        page.getByRole('heading', { name: upcomingEventTitle, level: 1 })
+      ).toBeVisible()
+    })
+  })
+
+  test.describe('Past events', () => {
+    test('should show the correct title', async ({ page }) => {
+      await expect(
+        page
+          .getByTestId('past-events-section')
+          .getByRole('heading', { name: 'Past events', level: 2 })
+      ).toBeVisible()
+    })
+
+    test('should select an upcoming event', async ({ page }) => {
+      const pastEvent = page
+        .getByTestId('past-events-section')
+        .getByTestId('event-card')
+        .first()
+      const pastEventTitle = await pastEvent.getByRole('heading').textContent()
+      await pastEvent.click()
+      await expect(
+        page.getByRole('heading', { name: pastEventTitle, level: 1 })
+      ).toBeVisible()
+    })
+  })
 })
