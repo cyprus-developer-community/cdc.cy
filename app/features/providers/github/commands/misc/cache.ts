a31f74fe5256014b89acf6a281d653a346b5cbc6
Federico Rampazzo <frampone@gmail.com>
2023-09-06 15:10:11 +0300

    chore: increase ttl to 20m

--- app/features/providers/github/commands/misc/cache.ts
+++ app/features/providers/github/commands/misc/cache.ts
@@ -28,7 +28,7 @@ cache.on('expired', (key) => {
 const fetchCached = async <T>(
   cacheKey: string,
   fetcher: () => Promise<T>,
-  ttl = 60 * 5
+  ttl = 60 * 20
 ) => {
   let response: T
   if (cache.has(cacheKey)) {
