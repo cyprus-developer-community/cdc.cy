e46d0afd65ac84769546dca935fbc23fca700515
Patrick Heneise <patrick@zentered.co>
2023-11-11 12:59:13 -0500

    fix: enable PAT auth

--- src/lib/octokit.server.js
+++ src/lib/octokit.server.js
@@ -1,17 +1,32 @@
 import { Octokit } from '@octokit/core'
 import { createAppAuth } from '@octokit/auth-app'
 
-const appOctokit = new Octokit({
-  authStrategy: createAppAuth,
-  auth: {
-    appId: process.env.GH_APP_ID,
-    privateKey: atob(process.env.GH_PRIVATE_KEY)
-  }
-})
+let token = '',
+  octokit = {}
 
-const { token } = await appOctokit.auth({
-  type: 'installation',
-  installationId: process.env.GH_APP_INSTALLATION_ID
-})
+if (process.env.GH_PAT) {
+  // with personal access token (local development)
+  const patOctokit = new Octokit({ auth: process.env.GH_PAT })
 
-export { token, appOctokit as octokit }
+  token = process.env.GH_PAT
+  octokit = patOctokit
+} else {
+  // with GitHub App (production)
+  const appOctokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: process.env.GH_APP_ID,
+      privateKey: atob(process.env.GH_PRIVATE_KEY)
+    }
+  })
+
+  const { token: appToken } = await appOctokit.auth({
+    type: 'installation',
+    installationId: process.env.GH_APP_INSTALLATION_ID
+  })
+
+  token = appToken
+  octokit = appOctokit
+}
+
+export { token, octokit }
