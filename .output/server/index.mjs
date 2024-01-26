b9b017d6063669fe1e1a8c4b4f47db4f20aec1b2
Patrick Heneise <patrick@zentered.co>
2024-01-26 21:34:40 +0200

    wip: upgrade solid start

---
+++ .output/server/index.mjs
@@ -0,0 +1,2 @@
+globalThis._importMeta_={url:"file:///_entry.js",env:{}};export{o as default}from"./chunks/nitro/cloudflare-module.mjs";import"__STATIC_CONTENT_MANIFEST";import"node:async_hooks";
+//# sourceMappingURL=index.mjs.map
--- src/lib/octokit.server.js
+++ src/lib/octokit.server.js
@@ -1,32 +1,33 @@
 import { Octokit } from '@octokit/core'
 import { createAppAuth } from '@octokit/auth-app'
 
-let token = '',
-  octokit = {}
+let token = ''
 
-if (process.env.GH_PAT) {
-  // with personal access token (local development)
-  const patOctokit = new Octokit({ auth: process.env.GH_PAT })
+async function octokit() {
+  if (process.env.GH_PAT) {
+    // with personal access token (local development)
+    const patOctokit = new Octokit({ auth: process.env.GH_PAT })
 
-  token = process.env.GH_PAT
-  octokit = patOctokit
-} else {
-  // with GitHub App (production)
-  const appOctokit = new Octokit({
-    authStrategy: createAppAuth,
-    auth: {
-      appId: process.env.GH_APP_ID,
-      privateKey: atob(process.env.GH_PRIVATE_KEY)
-    }
-  })
+    token = process.env.GH_PAT
+    return patOctokit
+  } else {
+    // with GitHub App (production)
+    const appOctokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: process.env.GH_APP_ID,
+        privateKey: atob(process.env.GH_PRIVATE_KEY)
+      }
+    })
 
-  const { token: appToken } = await appOctokit.auth({
-    type: 'installation',
-    installationId: process.env.GH_APP_INSTALLATION_ID
-  })
+    const { token: appToken } = await appOctokit.auth({
+      type: 'installation',
+      installationId: process.env.GH_APP_INSTALLATION_ID
+    })
 
-  token = appToken
-  octokit = appOctokit
+    token = appToken
+    return appOctokit
+  }
 }
 
-export { token, octokit }
+export { octokit, token }
