6c88173920096d85149a027c9dd706da8986c88a
Patrick Heneise <patrick@zentered.co>
2024-01-26 21:37:14 +0200

    fix: delete output

--- .output/server/index.mjs
+++
@@ -1,2 +0,0 @@
-globalThis._importMeta_={url:"file:///_entry.js",env:{}};export{o as default}from"./chunks/nitro/cloudflare-module.mjs";import"__STATIC_CONTENT_MANIFEST";import"node:async_hooks";
-//# sourceMappingURL=index.mjs.map
