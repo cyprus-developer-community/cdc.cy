f4706275d96df7e0dcf49f334e6e3d419bb18555
Patrick Heneise <patrick@zentered.co>
2024-02-09 12:21:56 +0100

    fix: try cf pages module

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from '@solidjs/start/config'
 export default defineConfig({
   start: {
     server: {
-      preset: 'cloudflare_module',
-      rollupConfig: {
-        external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks']
-      }
+      preset: 'cloudflare-pages'
     }
   }
 })
