48ee07ebd537d89d8d14304ac12b811142835289
Patrick Heneise <patrick@zentered.co>
2024-02-09 12:08:04 +0100

    fix: try latest versions

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,5 +1,5 @@
-import { createHandler } from '@solidjs/start/entry'
-import { StartServer } from '@solidjs/start/server'
+import { createHandler, StartServer } from '@solidjs/start/server'
+
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
