d42a1510fa8eeab9e144af561fc813b54ba0e04d
Patrick Heneise <patrick@zentered.co>
2024-02-29 19:43:39 +0000

    fix: solid 0.6 trial

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,20 +1,27 @@
-import { createHandler, StartServer } from '@solidjs/start/server'
+import { StartServer, createHandler } from '@solidjs/start/server'
 
-export default createHandler(() => (
-  <StartServer
-    document={({ assets, children, scripts }) => (
-      <html lang="en">
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-          {assets}
-        </head>
-        <body>
-          <div id="app">{children}</div>
-          {scripts}
-        </body>
-      </html>
-    )}
-  />
-))
+function Document(props) {
+  return (
+    <html lang="en" class="h-full">
+      <head>
+        <meta charset="utf-8" />
+        <meta property="og:title" content="Cyprus Developer Community" />
+        <meta property="og:site_name" content="Cyprus Developer Community" />
+        <meta property="og:url" content="https://cyc.cy" />
+        <meta property="og:description" content="" />
+        <meta property="og:type" content="website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" property="og:description" content="" />
+        <meta name="author" content="Cyprus Developer Community" />
+        <link rel="icon" href="/favicon.ico" />
+        {props.assets}
+      </head>
+      <body id="app" class="h-full">
+        {props.children}
+        {props.scripts}
+      </body>
+    </html>
+  )
+}
+
+export default createHandler(() => <StartServer document={Document} />)
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@solidjs/start/config'
-export default defineConfig({
-  start: {
-    server: {
-      preset: 'cloudflare-pages'
-    }
-  }
-})
